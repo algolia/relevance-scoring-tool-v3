7e95cf41b5fe9891dda09aea435dc673c82a0a43
Valentin Polo <valentin.polo@algolia.com>
2019-06-13 15:16:40 +0200

    Add server side Authentication mechanism

--- server.js
+++ server.js
@@ -1,69 +1,107 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 
-const algoliasearch = require('algoliasearch');
-const clientAlgolia = algoliasearch(process.env.RESULTS_APP_ID, process.env.RESULTS_ADMIN_API_KEY);
+const algoliasearch = require("algoliasearch");
+const clientAlgolia = algoliasearch(
+  process.env.RESULTS_APP_ID,
+  process.env.RESULTS_ADMIN_API_KEY
+);
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, "build")));
 app.use(express.json());
 app.use(cors());
 
-app.get('/api/settings', (req, res) => {
-    res.json({
-        appId: process.env.RECORDS_APP_ID,
-        searchApiKey: process.env.RECORDS_SEARCH_API_KEY,
-        indexName: process.env.RECORDS_INDEX_NAME,
-        attributesToDisplay: process.env.RECORDS_ATTRIBUTES.split(',').map(attr => attr.trim()),
-        imageAttribute: process.env.RECORDS_IMAGE_ATTRIBUTE,
-        queries: process.env.QUERIES.split(',').map(query => query.trim())
-    })
+app.get("/api/settings", (req, res) => {
+  res.json({
+    appId: process.env.RECORDS_APP_ID,
+    searchApiKey: process.env.RECORDS_SEARCH_API_KEY,
+    indexName: process.env.RECORDS_INDEX_NAME,
+    attributesToDisplay: process.env.RECORDS_ATTRIBUTES.split(",").map(attr =>
+      attr.trim()
+    ),
+    imageAttribute: process.env.RECORDS_IMAGE_ATTRIBUTE,
+    queries: process.env.QUERIES.split(",").map(query => query.trim())
+  });
 });
 
-app.post('/api/answers', (req, res) => {
-    const { appId, indexName, user, query, queryPosition, status, comment, timestamp } = req.body;
+app.post("/api/answers", (req, res) => {
+  const {
+    appId,
+    indexName,
+    user,
+    query,
+    queryPosition,
+    status,
+    comment,
+    timestamp
+  } = req.body;
 
-    const indexAlgolia = clientAlgolia.initIndex(`${appId}_${indexName}_${user}`);
+  const indexAlgolia = clientAlgolia.initIndex(`${appId}_${indexName}_${user}`);
 
-    indexAlgolia.addObject({
-        objectID: query,
-        query,
-        queryPosition,
-        status,
-        comment,
-        timestamp
-    }, (err, { objectID }) => {
-        res.json('Answer saved');
-    });
+  indexAlgolia.addObject(
+    {
+      objectID: query,
+      query,
+      queryPosition,
+      status,
+      comment,
+      timestamp
+    },
+    (err, { objectID }) => {
+      res.json("Answer saved");
+    }
+  );
 });
 
-app.get('/api/testers', function (req, res) {
-    clientAlgolia.listIndexes((err, content) => {
-        res.json(content.items.map(index => ({ tester: index.name, nbQueries: index.entries })));
-    })
+app.get("/api/testers", function(req, res) {
+  if (
+    !req.query.authCode ||
+    !process.env.RESULTS_AUTH_CODE ||
+    req.query.authCode !== process.env.RESULTS_AUTH_CODE
+  ) {
+    return res.status(401).json("Not authenticated");
+  }
+
+  clientAlgolia.listIndexes((err, content) => {
+    res.json(
+      content.items.map(index => ({
+        tester: index.name,
+        nbQueries: index.entries
+      }))
+    );
+  });
 });
 
-app.get('/api/results/:indexName', function (req, res) {
-    const indexName = req.params.indexName;
-    const indexAlgolia = clientAlgolia.initIndex(indexName);
-    const browser = indexAlgolia.browseAll();
+app.get("/api/results/:indexName", function(req, res) {
+  if (
+    !req.query.authCode ||
+    !process.env.RESULTS_AUTH_CODE ||
+    req.query.authCode !== process.env.RESULTS_AUTH_CODE
+  ) {
+    return res.status(401).json("Not authenticated");
+  }
 
-    let hits = [];
-    browser.on('result', content => hits = hits.concat(content.hits));
-    browser.on('end', () => res.json(hits));
+  const indexName = req.params.indexName;
+  const indexAlgolia = clientAlgolia.initIndex(indexName);
+  const browser = indexAlgolia.browseAll();
+
+  let hits = [];
+  browser.on("result", content => (hits = hits.concat(content.hits)));
+  browser.on("end", () => res.json(hits));
 });
 
-app.get('/api', function (req, res) {
-    res.json('Relevance Scoring Tool API v1')
+app.get("/api", function(req, res) {
+  res.json("Relevance Scoring Tool API v1");
 });
 
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname + '/build/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/build/index.html"));
 });
 
-app.listen(process.env.PORT || 8080, function () {
-    console.log('Relevance Scoring Tool API v1 started.')
+app.listen(process.env.PORT || 8080, function() {
+  console.log("Relevance Scoring Tool API v1 started.");
 });
--- src/Results.js
+++ src/Results.js
@@ -1,108 +1,136 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 import { CSVLink } from "react-csv";
+import queryString from "query-string";
 
 class Results extends React.Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
+    const queryParameters = queryString.parse(this.props.location.search);
 
-        this.state = {
-            selectedTester: false,
-            testers: [],
-            results: []
-        }
+    this.state = {
+      selectedTester: false,
+      testers: [],
+      results: [],
+      authenticated: null,
+      authCode: queryParameters.magic
+    };
+  }
+
+  componentDidMount() {
+    this.getTesters();
+  }
+
+  getTesters = () => {
+    axios.get(`/api/testers?authCode=${this.state.authCode}`).then(res => {
+      this.setState({ testers: res.data });
+    });
+  };
+
+  getResultsForTester = tester => {
+    axios
+      .get(`/api/results/${tester}?authCode=${this.state.authCode}`)
+      .then(res => {
+        this.setState({ results: res.data });
+      });
+  };
+
+  selectTester = selectedTester => {
+    this.setState({ selectedTester }, () =>
+      this.getResultsForTester(selectedTester)
+    );
+  };
+
+  render() {
+    const { testers, selectedTester, results, authenticated } = this.state;
+
+    if (authenticated !== true) {
+      return (
+        <>
+          <h3>Authentication required.</h3>
+          <p>You need to be logged-in to access this page.</p>
+        </>
+      );
     }
 
-    componentDidMount() {
-        this.getTesters();
-    }
+    return (
+      <React.Fragment>
+        <div className="row mb-4">
+          <div className="col">
+            <h3>Relevance Scoring</h3>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-3">
+            <h4>Testers</h4>
+            <ul>
+              {testers.map((tester, idx) => (
+                <li key={idx} onClick={() => this.selectTester(tester.tester)}>
+                  {tester.tester.split("_").pop()} ({tester.nbQueries})
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="col-9">
+            <h4>Results</h4>
+            {(!selectedTester || (selectedTester && !results.length)) && (
+              <p>No results to display.</p>
+            )}
 
-    getTesters = () => {
-        axios.get(`/api/testers`)
-            .then(res => {
-                this.setState({ testers: res.data });
-            })
-    };
-
-    getResultsForTester = tester => {
-        axios.get(`/api/results/${tester}`)
-            .then(res => {
-                this.setState({ results: res.data });
-            })
-    };
-
-    selectTester = selectedTester => {
-        this.setState({ selectedTester }, () => this.getResultsForTester(selectedTester));
-    };
-
-    render() {
-        const { testers, selectedTester, results } = this.state;
-
-        return (
-            <React.Fragment>
-                <div className="row mb-4">
-                    <div className="col">
-                        <h3>
-                            Relevance Scoring
-                        </h3>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-3">
-                        <h4>Testers</h4>
-                        <ul>
-                            {testers.map((tester, idx) => (
-                                <li key={idx} onClick={() => this.selectTester(tester.tester)}>
-                                    {tester.tester.split('_').pop()} ({tester.nbQueries})
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                    <div className="col-9">
-                        <h4>Results</h4>
-                        {(!selectedTester || (selectedTester && !results.length)) &&
-                            <p>No results to display.</p>
-                        }
-
-                        {selectedTester && results.length &&
-                        <React.Fragment>
-                            <p className="text-right mb-1">
-                                <CSVLink data={results} filename={`${selectedTester}.csv`}>
-                                    <small>Download as .csv file</small>
-                                </CSVLink>
-                            </p>
-                            <table className="table table-sm table-striped">
-                                <thead>
-                                <tr>
-                                    <th scope="col">Query</th>
-                                    <th scope="col">Status</th>
-                                    <th scope="col">Comment</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                {results.map((result, idx) => (
-                                    <tr key={idx}>
-                                        <th scope="row">{result.query}</th>
-                                        <td>
-                                            {result.status === 'RELEVANT' && <span className="badge badge-pill badge-success">{result.status}</span>}
-                                            {result.status === 'MISPLACED' && <span className="badge badge-pill badge-warning">{result.status}</span>}
-                                            {result.status === 'IRRELEVANT' && <span className="badge badge-pill badge-danger">{result.status}</span>}
-                                            {result.status === 'SKIPPED' && <span className="badge badge-pill badge-info">{result.status}</span>}
-                                        </td>
-                                        <td>{result.comment}</td>
-                                    </tr>
-                                ))}
-                                </tbody>
-                            </table>
-                        </React.Fragment>
-                        }
-                    </div>
-                </div>
-            </React.Fragment>
-        )
-    }
-
+            {selectedTester && results.length && (
+              <React.Fragment>
+                <p className="text-right mb-1">
+                  <CSVLink data={results} filename={`${selectedTester}.csv`}>
+                    <small>Download as .csv file</small>
+                  </CSVLink>
+                </p>
+                <table className="table table-sm table-striped">
+                  <thead>
+                    <tr>
+                      <th scope="col">Query</th>
+                      <th scope="col">Status</th>
+                      <th scope="col">Comment</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {results.map((result, idx) => (
+                      <tr key={idx}>
+                        <th scope="row">{result.query}</th>
+                        <td>
+                          {result.status === "RELEVANT" && (
+                            <span className="badge badge-pill badge-success">
+                              {result.status}
+                            </span>
+                          )}
+                          {result.status === "MISPLACED" && (
+                            <span className="badge badge-pill badge-warning">
+                              {result.status}
+                            </span>
+                          )}
+                          {result.status === "IRRELEVANT" && (
+                            <span className="badge badge-pill badge-danger">
+                              {result.status}
+                            </span>
+                          )}
+                          {result.status === "SKIPPED" && (
+                            <span className="badge badge-pill badge-info">
+                              {result.status}
+                            </span>
+                          )}
+                        </td>
+                        <td>{result.comment}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </React.Fragment>
+            )}
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Results;
